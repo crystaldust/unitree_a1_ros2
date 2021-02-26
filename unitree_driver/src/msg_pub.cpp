f63e2e4a20f34b5818977e3b2870d74591ca8e96
sun bo <sunbo32@huawei.com>
2021-02-26 20:25:50 +0800

    feat: add Walk function
    receive geometry_msg/Twist msgs and convert it into UNITREE highcmd,
    then send the high cmd to unitree A1.

--- unitree_driver/src/msg_pub.cpp
+++ unitree_driver/src/msg_pub.cpp
@@ -1,8 +1,43 @@
-
-
 //
 // Created by sun on 2021/2/24.
 //
 
+#include <memory>
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
+using namespace std::chrono_literals;
+
+class PubNode: public rclcpp::Node
+{
+public:
+    PubNode():Node("pub_node"), count_(0){
+        walk_pub = this->create_publisher<geometry_msgs::msg::Twist>("topic", 10);
+        timer_ = this->create_wall_timer(500ms, std::bind(&PubNode::pub_callback, this));
+    }
+
+private:
+    void pub_callback(){
+        geometry_msgs::msg::Twist msg;
+        msg.linear.x = 0.1;
+        msg.linear.y = 0.0;
+        msg.angular.z = 0.0;
+        RCLCPP_INFO(this->get_logger(), "sending");
+        walk_pub->publish(msg);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr walk_pub;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<PubNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+
+}
 
