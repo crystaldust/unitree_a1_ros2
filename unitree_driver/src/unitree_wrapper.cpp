18ccc79c3fa0be2f5fc249fbc4e4ed14af4ac099
sun bo <sunbo32@huawei.com>
2021-02-26 20:25:50 +0800

    add walk funcs

--- unitree_driver/src/unitree_wrapper.cpp
+++ unitree_driver/src/unitree_wrapper.cpp
@@ -2,22 +2,33 @@
 // Created by sun on 2021/2/24.
 //
 
+#include "unitree_driver/unitree_wrapper.h"
 
-class unitreeWrapper{
-public:
-    unitreeWrapper(){
+void unitreeWrapper::walkCmd(float forwardSpeed, float sideSpeed, float rotateSpeed){
 
+    UNITREE_LEGGED_SDK::HighState current_state;
+    UNITREE_LEGGED_SDK::HighCmd high_cmd = {0};
+
+    udp_wrapper.udp.GetRecv(current_state);
+    if (current_state.mode == 1){
+        std::cout << "received : 1 //" << current_state.mode << std::endl;
     }
 
-private:
-    void robotToLocalTime();
-    void walkCmd(float forwardSpeed, float sideSpeed, float rotateSpeed, float time);
-    void standCmd();
-    udp robot_command;
-};
+    high_cmd.mode = 2;
+    high_cmd.forwardSpeed = forwardSpeed;
+    high_cmd.sideSpeed = sideSpeed;
+    high_cmd.rotateSpeed = rotateSpeed;
 
-void unitreeWrapper::walkCmd(float forwardSpeed, float sideSpeed, float rotateSpeed, float time) {
-//    build udp command
-
-
+    udp_wrapper.udp.SetSend(high_cmd);
+    sleep(4);
+    stop();
 }
+
+void unitreeWrapper::stop() {
+    UNITREE_LEGGED_SDK::HighCmd stop_cmd = {0};
+    stop_cmd.mode = 1;
+    udp_wrapper.udp.SetSend(stop_cmd);
+}
+
+
+
