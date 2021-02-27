8ec72054142df2dca55d8ff88987bbbf99a138b7
sun bo <sunbo32@huawei.com>
2021-02-27 20:16:18 +0800

    fix: udp loog funcs exit error.
    UNITREE_LEGGED_SDK::LoopFunc should be implemented in the function which
    runs ROS nodes.

--- unitree_driver/src/unitree_ros.cpp
+++ unitree_driver/src/unitree_ros.cpp
@@ -10,7 +10,7 @@
 class unitreeROS : public rclcpp::Node
 {
 public:
-    unitreeROS():Node("node"), wrapper(unitreeWrapper()){
+    unitreeROS():Node("node"){
 
         sub = this->create_subscription<geometry_msgs::msg::Twist>(
                 "topic",
@@ -19,22 +19,31 @@ public:
                           this,
                           std::placeholders::_1)
                 );
-
     }
+    udpWrapper wrapper = udpWrapper();
 
 private:
 
-    void walkCallback(const geometry_msgs::msg::Twist::SharedPtr msg) {		
+    void walkCallback(const geometry_msgs::msg::Twist::SharedPtr msg) {
         wrapper.walkCmd(msg->linear.x, msg->linear.y, msg->angular.z);
     }
-   unitreeWrapper wrapper;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub;
 };
 
 
 int main(int argc, char* argv[]){
+    UNITREE_LEGGED_SDK::InitEnvironment();
+
+
     rclcpp::init(argc, argv);
     auto ros_node = std::make_shared<unitreeROS>();
+    float dt = 0.002f;
+
+    UNITREE_LEGGED_SDK::LoopFunc loop_udpRecv("udp_recv", dt, 3, boost::bind(&udpWrapper::UDPRecv, &ros_node->wrapper));
+    UNITREE_LEGGED_SDK::LoopFunc loop_udpSend("udp_send", dt, 3, boost::bind(&udpWrapper::UDPSend, &ros_node->wrapper));
+    loop_udpRecv.start();
+    loop_udpSend.start();
+
     rclcpp::spin(ros_node);
     rclcpp::shutdown();
     return 0;
--- unitree_driver/src/unitree_wrapper.cpp
+++ unitree_driver/src/unitree_wrapper.cpp
@@ -4,30 +4,48 @@
 
 #include "unitree_driver/unitree_wrapper.h"
 
-void unitreeWrapper::walkCmd(float forwardSpeed, float sideSpeed, float rotateSpeed){
+void udpWrapper::walkCmd(float forwardSpeed, float sideSpeed, float rotateSpeed){
 
     UNITREE_LEGGED_SDK::HighState current_state;
     UNITREE_LEGGED_SDK::HighCmd high_cmd = {0};
 
-    udp_wrapper.udp.GetRecv(current_state);
-    if (current_state.mode == 1){
-        std::cout << "received : 1 //" << current_state.mode << std::endl;
+    udp.GetRecv(current_state);
+    std::cout<<unsigned(current_state.mode)<<std::endl;
+
+    if (unsigned(current_state.mode) == 0){
+        std::cout << "mode : " << unsigned(current_state.mode) << std::endl;
+        high_cmd.mode = 1;
+        std::cout << "change mode 0 -> 1"<< std::endl;
+        udp.SetSend(high_cmd);
+        sleep(4);
     }
-
-    high_cmd.mode = 2;
-    high_cmd.forwardSpeed = forwardSpeed;
-    high_cmd.sideSpeed = sideSpeed;
-    high_cmd.rotateSpeed = rotateSpeed;
-
-    udp_wrapper.udp.SetSend(high_cmd);
-    sleep(4);
-    stop();
+    else if (unsigned(current_state.mode) == 1){
+        high_cmd.mode = 2;
+        std::cout << "change mode 1 -> 2"<< std::endl;
+        udp.SetSend(high_cmd);
+        sleep(4);
+    }
+    else if (unsigned(current_state.mode) ==2) {
+        std::cout << " walking ..."<< std::endl;
+        high_cmd.mode = 2;
+        high_cmd.forwardSpeed = forwardSpeed;
+        high_cmd.sideSpeed = sideSpeed;
+        high_cmd.rotateSpeed = rotateSpeed;
+        udp.SetSend(high_cmd);
+        sleep(2);
+        std::cout << " stop "<< std::endl;
+        stop();
+    }
+    else{
+        std::cout << "mode : " << unsigned(current_state.mode) << std::endl;
+    }
 }
 
-void unitreeWrapper::stop() {
+void udpWrapper::stop() {
     UNITREE_LEGGED_SDK::HighCmd stop_cmd = {0};
     stop_cmd.mode = 1;
-    udp_wrapper.udp.SetSend(stop_cmd);
+    udp.SetSend(stop_cmd);
+    sleep(10);
 }
 
 
