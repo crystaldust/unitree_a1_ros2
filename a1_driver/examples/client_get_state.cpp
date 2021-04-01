9067570324d95aac24de475f35c615dd1d95e3cd
guoliguo123 <13351616795@163.com>
2021-04-01 16:37:37 +0800

    Resolve compilation failure

--- a1_driver/examples/client_get_state.cpp
+++ a1_driver/examples/client_get_state.cpp
@@ -8,20 +8,20 @@ using namespace std::chrono_literals;
 
 class ClientNode : public rclcpp::Node {
 public:
-    ClientNode() : Node("GetImuMsg"), wait_count(5) {
-        client = this->create_client<a1_msgs::srv::Imu>(ROS2_TOPIC_GET_IMU_MSG);
-        timer_c = this->create_wall_timer(500ms, std::bind(&ClientNode::client_node_get_imu_msg, this));
+    ClientNode() : Node("HighState"), wait_count(5) {
+        client = this->create_client<a1_msgs::srv::HighState>(ROS2_TOPIC_GET_HIGH_STATE_MSG);
+        timer_c = this->create_wall_timer(500ms, std::bind(&ClientNode::client_node_get_high_state, this));
     }
 private:
-    void client_node_get_imu_msg();
+    void client_node_get_high_state();
     rclcpp::TimerBase::SharedPtr timer_c;
-    rclcpp::Client<a1_msgs::srv::Imu>::SharedPtr client;
+    rclcpp::Client<a1_msgs::srv::HighState>::SharedPtr client;
     size_t wait_times;
 };
 
-void ClientNode::client_node_get_imu_msg() {
+void ClientNode::client_node_get_high_state() {
     int times = 0;
-    auto request = std::make_shared<a1_msgs::srv::Imu::Request>();
+    auto request = std::make_shared<a1_msgs::srv::HighState::Request>();
     while (!client->wait_for_service(1s)) {
         times++;
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting times(%d)...", times);
@@ -34,8 +34,17 @@ void ClientNode::client_node_get_imu_msg() {
     if (rclcpp::spin_until_future_complete(node, result) ==
         rclcpp::executor::FutureReturnCode::SUCCESS)
     {
-        auto imu = result.get();
+        auto HighState = result.get();
         int i;
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "##############ROBOT BASIC INFO#######################");
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "levelFlag: %12u    commVersion: %12u", HighState->levelflag, HighState->commversion);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "robotID: %12u      SN: %12u", HighState->robotid, HighState->sn);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "mode: %12u         bandWidth: %12u", HighState->mode, HighState->bandwidth);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "##############ROBOT VELOCITY INFO#######################");
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "forwardSpeed: %12f   sideSpeed: %12f", HighState->forwardspeed, HighState->sidespeed);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "rotateSpeed: %12f    bodyHeight: %12f", HighState->rotateSpeed, HighState->bodyheight);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "updownSpeed: %12f    forwardPosition: %12f", HighState->updownspeed, HighState->forwardposition);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sidePosition: %12f", HighState->sideposition);
         
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "##############ROBOT IMU INFO#######################");
         for (i = 0; i < UNITREE_A1_IMU_QUATERNION; i++) {
@@ -50,6 +59,19 @@ void ClientNode::client_node_get_imu_msg() {
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "imu.gyroscope[%d]: %12f", i, HighState->imu.accelerometer[i]);
         }
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "imu.temperature: %12f", HighState->imu.temperature);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "#################ROBOT FOUR LEGS INFO####################");
+        for(i = 0; i< UNITREE_A1_DOG_LEGS; i++) {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footposition2body[i].x: %12f", i, HighState->footposition2body[i].x);
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footposition2body[i].y: %12f", i, HighState->footposition2body[i].y);
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footposition2body[i].z: %12f", i, HighState->footposition2body[i].z);
+            
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footspeed2body[i].x: %12f", i, HighState->footspeed2body[i].x);
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footspeed2body[i].y: %12f", i, HighState->footspeed2body[i].y);
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footspeed2body[i].z: %12f", i, HighState->footspeed2body[i].z);
+
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footforce[i]: %12f", i, HighState->footforce[i]);
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "footforceest[i]: %12f", i, HighState->footforceest[i]);
+        }
     } 
     else {
         RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service");
