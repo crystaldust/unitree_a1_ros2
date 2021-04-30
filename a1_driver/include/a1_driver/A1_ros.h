03810aadf69cebcaa423263837d46520fd633aef
guoliguo123 <13351616795@163.com>
2021-04-30 17:05:27 +0800

    Modify code style

--- a1_driver/include/a1_driver/A1_ros.h
+++ a1_driver/include/a1_driver/A1_ros.h
@@ -13,43 +13,43 @@
 #include "a1_msgs/srv/cartesian.hpp"
 #include "a1_msgs/srv/high_state.hpp"
 #include "a1_msgs/srv/imu.hpp"
-#include "a1_msgs/srv/mode.hpp"
 #include "a1_msgs/srv/low_state.hpp"
+#include "a1_msgs/srv/mode.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 class A1ROS {
-public:
-    A1ROS(std::string node_name, int level) : wrapper(A1Wrapper(level)) {
-        this->node_name = node_name;
-    }
-    A1ROS(std::string node_name, uint8_t sport_mode, int level)
-            : wrapper(A1Wrapper(sport_mode, level)) {
-        this->node_name = node_name;
-    }
+ public:
+  A1ROS(std::string node_name, int level) : wrapper(A1Wrapper(level)) {
+    this->node_name = node_name;
+  }
+  A1ROS(std::string node_name, uint8_t sport_mode, int level)
+      : wrapper(A1Wrapper(sport_mode, level)) {
+    this->node_name = node_name;
+  }
 
-    int node_init(int argc, char *argv[]);
-    void get_high_state_msg(
-            std::shared_ptr<a1_msgs::srv::HighState::Response> response);
-    void get_low_state_msg(
-            std::shared_ptr<a1_msgs::srv::LowState::Response> response);
-    void get_imu_msg(std::shared_ptr<a1_msgs::srv::Imu::Response> response);
-    void get_cartesian_msg(
-            std::shared_ptr<a1_msgs::srv::Cartesian::Response> response);
-    A1Wrapper wrapper;
+  int node_init(int argc, char *argv[]);
+  void get_high_state_msg(
+      std::shared_ptr<a1_msgs::srv::HighState::Response> response);
+  void get_low_state_msg(
+      std::shared_ptr<a1_msgs::srv::LowState::Response> response);
+  void get_imu_msg(std::shared_ptr<a1_msgs::srv::Imu::Response> response);
+  void get_cartesian_msg(
+      std::shared_ptr<a1_msgs::srv::Cartesian::Response> response);
+  A1Wrapper wrapper;
 
-private:
-    std::string node_name;
-    using ModeRequest = std::shared_ptr<a1_msgs::srv::Mode::Request>;
-    using ModeResponse = std::shared_ptr<a1_msgs::srv::Mode::Response>;
-    using HighStateRequest = std::shared_ptr<a1_msgs::srv::HighState::Request>;
-    using HighStateResponse = std::shared_ptr<a1_msgs::srv::HighState::Response>;
-    using LowStateRequest = std::shared_ptr<a1_msgs::srv::LowState::Request>;
-    using LowStateResponse = std::shared_ptr<a1_msgs::srv::LowState::Response>;
-    using ImuRequest = std::shared_ptr<a1_msgs::srv::Imu::Request>;
-    using ImuResponse = std::shared_ptr<a1_msgs::srv::Imu::Response>;
-    using CartesianRequest = std::shared_ptr<a1_msgs::srv::Cartesian::Request>;
-    using CartesianResponse = std::shared_ptr<a1_msgs::srv::Cartesian::Response>;
+ private:
+  std::string node_name;
+  using ModeRequest = std::shared_ptr<a1_msgs::srv::Mode::Request>;
+  using ModeResponse = std::shared_ptr<a1_msgs::srv::Mode::Response>;
+  using HighStateRequest = std::shared_ptr<a1_msgs::srv::HighState::Request>;
+  using HighStateResponse = std::shared_ptr<a1_msgs::srv::HighState::Response>;
+  using LowStateRequest = std::shared_ptr<a1_msgs::srv::LowState::Request>;
+  using LowStateResponse = std::shared_ptr<a1_msgs::srv::LowState::Response>;
+  using ImuRequest = std::shared_ptr<a1_msgs::srv::Imu::Request>;
+  using ImuResponse = std::shared_ptr<a1_msgs::srv::Imu::Response>;
+  using CartesianRequest = std::shared_ptr<a1_msgs::srv::Cartesian::Request>;
+  using CartesianResponse = std::shared_ptr<a1_msgs::srv::Cartesian::Response>;
 };
 
 #endif  // A1_DRIVER__A1_ROS_H_
--- a1_driver/include/a1_driver/A1_wrapper.h
+++ a1_driver/include/a1_driver/A1_wrapper.h
@@ -13,51 +13,46 @@
 #define RECV_LENGTH UNITREE_LEGGED_SDK::HIGH_CMD_LENGTH
 
 class A1Wrapper {
-public:
-    /*sport mode*/
-    A1Wrapper(uint8_t sport_mode, int set_level)
-            : udp(8081, "192.168.123.161", 8082, SEND_LENGTH, RECV_LENGTH),
-              safety(UNITREE_LEGGED_SDK::LeggedType::A1) {
-      r_mode = sport_mode;
-      level = set_level;
-      if (level == UNITREE_LEGGED_SDK::HIGHLEVEL) {
-        udp.InitCmdData(highCmd);
-      }
-      else {
-        udp.InitCmdData(lowCmd);
-      }
+ public:
+  /*sport mode*/
+  A1Wrapper(uint8_t sport_mode, int set_level)
+      : udp(8081, "192.168.123.161", 8082, SEND_LENGTH, RECV_LENGTH),
+        safety(UNITREE_LEGGED_SDK::LeggedType::A1) {
+    r_mode = sport_mode;
+    level = set_level;
+    if (level == UNITREE_LEGGED_SDK::HIGHLEVEL) {
+      udp.InitCmdData(highCmd);
+    } else {
+      udp.InitCmdData(lowCmd);
     }
-    /*basic mode*/
-    explicit A1Wrapper(int set_level)
-            : udp(set_level),
-              safety(UNITREE_LEGGED_SDK::LeggedType::A1),
-              r_mode(0) {
-      level = set_level;
-      if (level == UNITREE_LEGGED_SDK::HIGHLEVEL) {
-        udp.InitCmdData(highCmd);
-      }
-      else {
-        udp.InitCmdData(lowCmd);
-      }
+  }
+  /*basic mode*/
+  explicit A1Wrapper(int set_level)
+      : udp(set_level), safety(UNITREE_LEGGED_SDK::LeggedType::A1), r_mode(0) {
+    level = set_level;
+    if (level == UNITREE_LEGGED_SDK::HIGHLEVEL) {
+      udp.InitCmdData(highCmd);
+    } else {
+      udp.InitCmdData(lowCmd);
     }
-    void UDPRecv() { udp.Recv(); }
-    void UDPSend() { udp.Send(); }
-    void set_velocity(float forwardSpeed, float sideSpeed, float rotateSpeed);
-    void set_pose(float yaw, float pitch, float roll, float bodyHeight);
-    bool set_mode(uint8_t mode);
-    void recv_high_state();
-    void recv_low_state();
-    void recv_imu_msg();
-    void recv_cartesian_msg();
-    UNITREE_LEGGED_SDK::UDP udp;
-    UNITREE_LEGGED_SDK::Safety safety;
-    UNITREE_LEGGED_SDK::HighCmd highCmd {};
-    UNITREE_LEGGED_SDK::HighState highState;
-    UNITREE_LEGGED_SDK::LowCmd lowCmd {};
-    UNITREE_LEGGED_SDK::LowState lowState;
-    uint8_t r_mode;
-    int level;
+  }
+  void UDPRecv() { udp.Recv(); }
+  void UDPSend() { udp.Send(); }
+  void set_velocity(float forwardSpeed, float sideSpeed, float rotateSpeed);
+  void set_pose(float yaw, float pitch, float roll, float bodyHeight);
+  bool set_mode(uint8_t mode);
+  void recv_high_state();
+  void recv_low_state();
+  void recv_imu_msg();
+  void recv_cartesian_msg();
+  UNITREE_LEGGED_SDK::UDP udp;
+  UNITREE_LEGGED_SDK::Safety safety;
+  UNITREE_LEGGED_SDK::HighCmd highCmd{};
+  UNITREE_LEGGED_SDK::HighState highState;
+  UNITREE_LEGGED_SDK::LowCmd lowCmd{};
+  UNITREE_LEGGED_SDK::LowState lowState;
+  uint8_t r_mode;
+  int level;
 };
 
 #endif  // A1_DRIVER__A1_WRAPPER_H_
-
